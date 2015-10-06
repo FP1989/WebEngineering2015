3d143ae46960c01309b1f366fb5e0d1adce7fe8f
Jack <florian.perrinjaquet@students.fhnw.ch>
2015-10-06 13:42:57 +0200

    New file, including queries on both (DRY)

---
+++ reports_pdf.php
@@ -0,0 +1,13 @@
+<?php
+
+include ("htmltopdf/html2pdf.class.php");
+require ("db_connect.php");
+
+session_start();
+
+ob_start();
+require ("reports_query.php");
+$content = ob_get_clean();
+$pdf = new HTML2PDF('P', 'A4', 'en', 'true', 'UTF-8');
+$pdf->writeHTML($content);
+$pdf->Output('table.pdf');
