1504fdb84cd953b4946938e240bb2ee8c8bef3b0
Jack <florian.perrinjaquet@students.fhnw.ch>
2015-10-07 12:23:08 +0200

    New file

--- reports_pdf.php
+++ reports_pdf.php
@@ -8,6 +8,6 @@ session_start();
 ob_start();
 require ("reports_query.php");
 $content = ob_get_clean();
-$pdf = new HTML2PDF('P', 'A4', 'en', 'true', 'UTF-8');
+$pdf = new HTML2PDF('L', 'A4', 'en', 'true', 'UTF-8');
 $pdf->writeHTML($content);
 $pdf->Output('table.pdf');
