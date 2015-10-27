8196e9ba2b507ce7d4ffc3887e899bf9fc769bb9
Jack <florian.perrinjaquet@students.fhnw.ch>
2015-10-28 00:30:15 +0100

    Einige Layout & Design Änderungen

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,4 +1,4 @@
-            <div id="footer">
+            <div class="navbar navbar-default navbar-fixed-bottom" id="footer">
                 <p>Star Reisen AG &copy 2015</p>
             </div>
         </div> <!-- end #wrapper -->
--- includes/navigation.inc.php
+++ includes/navigation.inc.php
@@ -1,10 +1,10 @@
 
 <div id="navigation" class="navbar navbar-default">
     <ul class="nav navbar-nav">
-        <li><a href="home.php"><i class="fa fa-home fa-2x"></i> Home</a></li>
-        <li><a href="rechnungen.php"><i class="fa fa-balance-scale fa-2x"></i> Rechnungen</a></li>
-        <li><a href="reisen.php"><i class="fa fa-plane fa-2x"></i> Reisen</a></li>
-        <li><a href="teilnehmer.php"><i class="fa fa-users fa-2x"></i> Teilnehmer</a></li>
-        <li><a href="reports_index.php"><i class="fa fa-table fa-2x"></i> Reports</a></li>
+        <li><a href="home.php"><i class="fa fa-home fa-2x"></i><br>Home</a></li>
+        <li><a href="rechnungen.php"><i class="fa fa-balance-scale fa-2x"></i><br>Rechnungen</a></li>
+        <li><a href="reisen.php"><i class="fa fa-plane fa-2x"></i><br>Reisen</a></li>
+        <li><a href="teilnehmer.php"><i class="fa fa-users fa-2x"></i><br>Teilnehmer</a></li>
+        <li><a href="reports_index.php"><i class="fa fa-table fa-2x"></i><br>Reports</a></li>
     </ul>
 </div>
