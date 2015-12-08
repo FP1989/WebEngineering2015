4e592a378cf7b5f3b2c7c85f392c89d057d8ed8d
Tobias <tobias.hofmann@students.fhnw.ch>
2015-12-08 10:10:14 +0100

    validation bugs behoben

--- teilnehmer.write.php
+++ teilnehmer.write.php
@@ -11,35 +11,35 @@ $error_alert="";
 if(isset($_POST['gesendet'])) {
     if (empty($_POST['surname'])) {
         $surname_error = "Bitte einen Vornamen eingeben";
-        $valid="false";
+        $valid=false;
     }
     if (empty($_POST['lastname'])) {
         $lastname_error = "Bitte einen Nachname eingeben";
-        $valid="false";
+        $valid=false;
     }
     if (empty($_POST['street'])) {
         $street_error = "Bitte eine Strasse eingeben";
-        $valid="false";
+        $valid=false;
     }
     if (empty($_POST['housenumber'])) {
         $housenumber_error = "Bitte eine Strasse eingeben";
-        $valid="false";
+        $valid=false;
     }
     if (empty($_POST['plz'])) {
         $plz_error = "Bitte eine PLZ eingeben";
-        $valid="false";
+        $valid=false;
     }
     if (empty($_POST['town'])) {
         $town_error = "Bitte eine Stadt eingeben";
-        $valid="false";
+        $valid=false;
     }
     if (empty($_POST['telefon'])) {
         $telefon_error = "Bitte eine Strasse eingeben";
-        $valid="false";
+        $valid=false;
     }
     if (empty($_POST['email'])) {
         $email_error = "Bitte eine E-Mail Adresse eingeben";
-        $valid="false";
+        $valid=false;
     }
     if ($valid) {
         $participantData = array();
@@ -57,9 +57,22 @@ if(isset($_POST['gesendet'])) {
         //make insert-statement
         /** @var database $verbindung */
         $verbindung = database::getDatabase();
-        $successful = $verbindung->insertTeilnehmner($participant);
+        $command = $verbindung->insertTeilnehmner($participant);
+        $num_rows = $command->num_rows;
+        if($num_rows > 0){
+            unset($_POST['surname']);
+            unset($_POST['lastname']);
+            unset($_POST['street']);
+            unset($_POST['housenumber']);
+            unset($_POST['plz']);
+            unset($_POST['town']);
+            unset($_POST['telefon']);
+            unset($_POST['email']);
+            $success_alert= "<div class='alert alert-success' role='alert'>Neuen Teilnehmer erfasst.</div>";
+        }else{
+            $error_alert = "<div class='alert alert-warning' role='alert'>Datenbank-Befehl fehlgeschlagen.</div>";
+        }
 
-        $success_alert= "<div class='alert alert-success' role='alert'>Neuen Teilnehmer erfasst.</div>";
     }else{
         $error_alert = "<div class='alert alert-warning' role='alert'>Das Formular enthält Fehler/Unvollständigkeiten.</div>";
     }
@@ -90,13 +103,13 @@ if(isset($_POST['gesendet'])) {
 
     <div class="form-group <?php echo (!empty($surname_error)) ? 'has-error':''; ?>">
         <label>Vorname</label>
-        <input class="form-control" name="surname" type="text">
+        <input class="form-control" name="surname" type="text" value="<?php echo @$_POST['surname'];?>">
         <?php echo "<span class='help-block'>$surname_error</span>";?>
     </div>
 
     <div class="form-group <?php echo (!empty($lastname_error)) ? 'has-error':''; ?>">
         <label>Nachname</label>
-        <input class="form-control" name="lastname" type="text">
+        <input class="form-control" name="lastname" type="text" value="<?php echo @$_POST['lastname'];?>">
         <?php echo "<span class='help-block'>$lastname_error</span>";?>
     </div>
 
@@ -104,12 +117,12 @@ if(isset($_POST['gesendet'])) {
         <div class="row">
             <div class="col-md-8 <?php echo (!empty($street_error)) ? 'has-error':''; ?>">
                 <label>Strasse</label>
-                <input class="form-control" name="street" type="text">
+                <input class="form-control" name="street" type="text" value="<?php echo @$_POST['street'];?>">
                 <?php echo "<span class='help-block'>$street_error</span>";?>
             </div>
             <div class="col-md-4 <?php echo (!empty($housenumber_error)) ? 'has-error':''; ?>">
                 <label>Hausnummer</label>
-                <input class="form-control" name="housenumber" type="number">
+                <input class="form-control" name="housenumber" type="number" value="<?php echo @$_POST['housenumber'];?>">
                 <?php echo "<span class='help-block'>$housenumber_error</span>";?>
             </div>
         </div>
@@ -119,12 +132,12 @@ if(isset($_POST['gesendet'])) {
         <div class="row">
             <div class="col-md-4 <?php echo (!empty($plz_error)) ? 'has-error':''; ?>">
                 <label>PLZ</label>
-                <input class="form-control" name="plz" type="text">
+                <input class="form-control" name="plz" type="text" value="<?php echo @$_POST['plz'];?>">
                 <?php echo "<span class='help-block'>$plz_error</span>";?>
             </div>
             <div class="col-md-8 <?php echo (!empty($town_error)) ? 'has-error':''; ?>">
                 <label>Ort</label>
-                <input class="form-control" name="town" type="text">
+                <input class="form-control" name="town" type="text" value="<?php echo @$_POST['town'];?>">
                 <?php echo "<span class='help-block'>$town_error</span>";?>
             </div>
         </div>
@@ -133,13 +146,13 @@ if(isset($_POST['gesendet'])) {
 
     <div class="form-group <?php echo (!empty($telefon_error)) ? 'has-error':''; ?>">
         <label>Telefon Nr.</label>
-        <input class="form-control" name="telefon" type="number">
+        <input class="form-control" name="telefon" type="number" value="<?php echo @$_POST['telefon'];?>">
         <?php echo "<span class='help-block'>$telefon_error</span>";?>
     </div>
 
     <div class="form-group <?php echo (!empty($email_error)) ? 'has-error':''; ?>">
         <label>E-Mail Adresse</label>
-        <input class="form-control" name="email" type="text">
+        <input class="form-control" name="email" type="text" value="<?php echo @$_POST['email'];?>">
         <?php echo "<span class='help-block'>$email_error</span>";?>
     </div>
 
